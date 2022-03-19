a6fec2a94c345cd64a8b658a3cfcf09668fe356a
berton7 <francy.berton99@gmail.com>
2022-03-19 09:33:45 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 sudo pacman -Syu zsh --needed
 [ ! -d "ttf-meslo-nerd-font-powerlevel10k" ] && git clone https://aur.archlinux.org/ttf-meslo-nerd-font-powerlevel10k.git --depth 1 && cd ttf-meslo-nerd-font-powerlevel10k && makepkg -si && cd ..
-mkdir -p "/home/$USER/.config/zsh"
-[ ! -d "/home/$USER/.config/zsh/.oh-my-zsh" ] && ZSH="/home/$USER/.config/zsh/.oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-[ ! -d "/home/$USER/.config/zsh/.oh-my-zsh/custom/themes/powerlevel10k" ] && git clone https://github.com/romkatv/powerlevel10k.git "/home/$USER/.config/zsh/.oh-my-zsh/custom/themes/powerlevel10k" --depth 1
-[ ! -d "/home/$USER/.config/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "/home/$USER/.config/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" --depth 1
-[ ! -d "/home/$USER/.config/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions "/home/$USER/.config/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions" --depth 1
-cp zprofile "/home/$USER/.zprofile"
-cp zshrc "/home/$USER/.config/zsh/.zshrc"
-cp p10k.zsh "/home/$USER/.config/zsh/.p10k.zsh"
-mkdir -p "/home/$USER/.config/shell"
-cp aliasrc "/home/$USER/.config/shell"
-cp profile "/home/$USER/.config/shell"
+mkdir -p "$HOME/.config/zsh"
+[ ! -d "$HOME/.config/zsh/.oh-my-zsh" ] && ZSH="$HOME/.config/zsh/.oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+[ ! -d "$HOME/.config/zsh/.oh-my-zsh/custom/themes/powerlevel10k" ] && git clone https://github.com/romkatv/powerlevel10k.git "$HOME/.config/zsh/.oh-my-zsh/custom/themes/powerlevel10k" --depth 1
+[ ! -d "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" --depth 1
+[ ! -d "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions" --depth 1
+ln -s "$HOME/.config/shell/profile" "$HOME/.zprofile"
+cp zshrc "$HOME/.config/zsh/.zshrc"
+cp p10k.zsh "$HOME/.config/zsh/.p10k.zsh"
+mkdir -p "$HOME/.config/shell"
+cp aliasrc "$HOME/.config/shell"
+cp profile "$HOME/.config/shell"
 
 echo Script done. "zsh -l" to take a look at your new shell!
