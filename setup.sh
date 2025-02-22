ddef2e26c4d8aee562819011b2ccff622d2a97f9
berton7 <francy.berton99@gmail.com>
2025-02-22 13:32:22 +0100

    Updated setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install zsh
 mkdir -p "$HOME/.config/zsh"
-[ ! -d "$HOME/.config/zsh/.oh-my-zsh" ] && ZSH="$HOME/.config/zsh/.oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+[ ! -d "$HOME/.config/zsh/.oh-my-zsh" ] && ZSH="$HOME/.config/zsh/.oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 [ ! -d "$HOME/.config/zsh/.oh-my-zsh/custom/themes/powerlevel10k" ] && git clone https://github.com/romkatv/powerlevel10k.git "$HOME/.config/zsh/.oh-my-zsh/custom/themes/powerlevel10k" --depth 1
 [ ! -d "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" --depth 1
 [ ! -d "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.config/zsh/.oh-my-zsh/custom/plugins/zsh-autosuggestions" --depth 1
